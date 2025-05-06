7463d68bd1f9a27326906ab7ec87ad0839253124
zhesun-0209 <157095263+zhesun-0209@users.noreply.github.com>
2025-05-06 13:16:08 -0400

    Update ml_models.py

--- models/ml_models.py
+++ models/ml_models.py
@@ -6,11 +6,10 @@ from sklearn.multioutput import MultiOutputRegressor
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
 
-def train_rf(
-    X_train, y_train, params: dict
-):
-    """Train Random Forest regressor."""
-    model = RandomForestRegressor(**params)
+def train_rf(X_train, y_train, params: dict):
+    """Train Random Forest regressor with multi-output support."""
+    base = RandomForestRegressor(**params)
+    model = MultiOutputRegressor(base)
     model.fit(X_train, y_train)
     return model
 
